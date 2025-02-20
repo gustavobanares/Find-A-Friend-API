28ef333f31722afcedde3821a7e08bc7026ed87b
*Gustavo <gustavobbacco@gmail.com>
2025-02-20 19:29:16 -0300

    feat: creating jwt

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,43 @@
 import fastify from "fastify";
 import { petsRoutes } from "./http/controllers/pets/routes";
 import { organizationRoutes } from "./http/controllers/organization/routes";
+import fastifyJwt from "@fastify/jwt";
+import fastifyCookie from "@fastify/cookie";
+import { ZodError } from "zod";
+import { env } from "./env";
 
-export const app = fastify()
+export const app = fastify();
 
+const jwtSecret = env.JWT_SECRET
 
-app.register(petsRoutes)
-app.register(organizationRoutes)
+app.register(fastifyJwt, {
+  secret: jwtSecret,
+  cookie: {
+    cookieName: "refreshToken",
+    signed: false,
+  },
+  sign: {
+    expiresIn: "10m",
+  },
+});
+
+app.register(fastifyCookie)
+
+app.register(petsRoutes);
+app.register(organizationRoutes);
+
+app.setErrorHandler((error, _, reply) => {
+    if (error instanceof ZodError) {
+      return reply
+        .status(400)
+        .send({ message: "Validation error.", issues: error.format() });
+    }
+  
+    if (env.NODE_ENV !== "production") {
+      console.error(error);
+    } else {
+    }
+  
+    return reply.status(500).send({ message: "Internal server error" });
+  });
+

b806bcf512f1c7ab7d35e8d00a9a91b273eee0b7
*Gustavo <gustavobbacco@gmail.com>
2025-02-21 20:00:52 -0300

    routes to filter and list

--- src/http/controllers/pets/routes.ts
+++ src/http/controllers/pets/routes.ts
@@ -2,10 +2,13 @@ import { FastifyInstance } from "fastify";
 import { create } from "./create";
 import { verifyJWT } from "@/http/middlewares/verify-jwt";
 import { details } from "./details";
+import { filter } from "./filter";
+import { list } from "./list";
 
 export async function petsRoutes(app: FastifyInstance) {
   app.post("/pets", {onRequest: [verifyJWT]}, create);
 
   app.get('/pets/:id', details)
-
+  app.get('/pets', filter)
+  app.get('/pets/all', list)
 }

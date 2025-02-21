8445211224b26e6de8cc139edbba822ad358de16
*Gustavo <gustavobbacco@gmail.com>
2025-02-21 19:22:04 -0300

    feat: route to get a pet

--- src/http/controllers/pets/routes.ts
+++ src/http/controllers/pets/routes.ts
@@ -1,7 +1,11 @@
 import { FastifyInstance } from "fastify";
 import { create } from "./create";
+import { verifyJWT } from "@/http/middlewares/verify-jwt";
+import { details } from "./details";
 
 export async function petsRoutes(app: FastifyInstance) {
-  app.post("/pets", create);
+  app.post("/pets", {onRequest: [verifyJWT]}, create);
+
+  app.get('/pets/:id', details)
 
 }
